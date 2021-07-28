79d97c3ac2a5767f3713efc1e08a92c23187f33f
Deepak Yadav <deepakyadav1331@gmail.com>
2021-07-29 03:57:36 +0530

    Added Entity, service and controllers

---
+++ src/main/java/com/learn/controller/ProductController.java
@@ -0,0 +1,51 @@
+package com.learn.controller;
+
+import com.learn.entity.Product;
+import com.learn.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("rest/product")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @PostMapping("/addProduct")
+    public Product addProduct(@RequestBody Product product){
+        return service.saveProduct(product);
+    }
+
+    @PostMapping("/addProducts")
+    public List<Product> addProducts(@RequestBody List<Product> products){
+        return service.saveProducts(products);
+    }
+
+    @GetMapping("/")
+    public List<Product> getAllProducts(){
+        return service.getAllProducts();
+    }
+
+    @GetMapping("/id/{id}")
+    public Product getProductById(@PathVariable int id){
+        return service.getProductById(id);
+    }
+
+    @GetMapping("/name/{name}")
+    public Product getProductByName(@PathVariable String name){
+        return service.getProductByName(name.trim());
+    }
+
+    @PutMapping("/update")
+    public Product updateProduct(@RequestBody Product product){
+        return service.updateProduct(product);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteProductById(@PathVariable int id){
+        return service.deleteProduct(id);
+    }
+}
--- src/main/java/com/learn/entity/Product.java
+++ src/main/java/com/learn/entity/Product.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 
 
 @Entity
@@ -15,6 +16,7 @@ import lombok.NoArgsConstructor;
 public class Product {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private int quantity;
